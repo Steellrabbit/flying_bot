663745719d1947ea64a01c0c09866b7dcc2a07b3
Alexander Kostarev <akostarev@diasoft.ru>
2023-01-06 01:11:28 +0300

    set excel formatting

--- config.py
+++ config.py
@@ -11,3 +11,38 @@ RUNTIME_FOLDER = 'assets/runtime'
 # folders inside RUNTIME_FOLDER
 TESTS_FOLDERNAME = 'tests'
 WRITTEN_TESTS_FOLDERNAME = 'results'
+
+##### Excel formatting #####
+HEADING_FORMAT = {
+        'bold': True,
+        'font_size': 11,
+        }
+
+CENTERED_HEADING_FORMAT = {
+        **HEADING_FORMAT,
+        'align': 'center',
+        }
+
+RIGHT_BORDERED_FORMAT = {
+        'right': 1
+        }
+
+BOTTOM_BORDERED_FORMAT = {
+        'bottom': 1
+        }
+
+COLUMN_BG_COLORED_FORMAT = {
+        'pattern': 2,
+        'bg_color': 'orange',
+        }
+
+ROW_BG_COLORED_FORMAT = {
+        'pattern': 4,
+        'bg_color': 'gold',
+        }
+
+WRAPPED_FORMAT = {
+        'text_wrap': True
+        }
+DEFAULT_FONT_SIZE = 10
+DEFAULT_FONT_NAME = 'Arial'
--- src/services/excels.py
+++ src/services/excels.py
@@ -6,9 +6,12 @@ import xlsxwriter as xls
 import xlsxwriter.worksheet as xls_worksheet
 import xlsxwriter.utility as xls_utility
 
-from config import RUNTIME_FOLDER, WRITTEN_TESTS_FOLDERNAME
+from config import BOTTOM_BORDERED_FORMAT, CENTERED_HEADING_FORMAT,\
+        COLUMN_BG_COLORED_FORMAT, DEFAULT_FONT_NAME, DEFAULT_FONT_SIZE,\
+        HEADING_FORMAT, RIGHT_BORDERED_FORMAT, ROW_BG_COLORED_FORMAT,\
+        RUNTIME_FOLDER, WRAPPED_FORMAT, WRITTEN_TESTS_FOLDERNAME
 
-from ..models.excel import WrittenTestExcel,\
+from ..models.excel import WrittenTestExcel, WrittenTestGroup,\
         WrittenTestQuestionData,\
         WrittenTestStudentData,\
         UpdatedTestExcel,\
@@ -110,58 +113,70 @@ class ExcelService():
         """Writes test results into excel and returns excel filename"""
         filename = f'{RUNTIME_FOLDER}/{WRITTEN_TESTS_FOLDERNAME}/{test.name}_{test.date}.xlsx'
         book = xls.Workbook(filename)
+        book.formats[0].set_font_size(DEFAULT_FONT_SIZE)
+        book.formats[0].set_font_name(DEFAULT_FONT_NAME)
 
-        student_mark_cells = dict()
+        student_mark_cells: dict[str, dict[str, str]] = dict()
         for variant in test.variants:
             sheet = book.add_worksheet(variant.name)
-            self.__write_questions(sheet, variant.questions)
-            mark_cells = self.__write_students(sheet, variant.students, len(variant.questions))
+            sheet.freeze_panes(2, 2)
+            self.__write_questions(book, sheet, variant.questions)
+            mark_cells = self.__write_students(book, sheet, variant.students, len(variant.questions))
             for key in mark_cells.keys():
                 student_mark_cells[key] = { 'mark_cell': mark_cells[key], 'sheet': variant.name }
 
-        sheet = book.add_worksheet('группы')
-        row_offset = 0
-        for group in test.summary.groups:
-            sheet.write(0 + row_offset, 0, group.group)
-            sheet.write(0 + row_offset, 1, 'студент')
-            sheet.write(0 + row_offset, 2, 'балл')
-            row_offset += 1
-            student_number = 1
-            for student in group.students:
-                mark_cell = student_mark_cells[str(student.id)]
-                sheet.write(0 + row_offset, 0, student_number)
-                sheet.write(0 + row_offset, 1, student.name)
-                sheet.write_formula(0 + row_offset, 2, f"'{mark_cell['sheet']}'!{mark_cell['mark_cell']}")
-                student_number += 1
-                row_offset += 1
-            row_offset += 1
+        sheet = book.add_worksheet('Группы')
+        self.__write_summary(book, sheet, test.summary.groups, student_mark_cells)
 
         book.close()
         return filename
 
     def __write_questions(self,
+            book: xls.Workbook,
             sheet: xls_worksheet.Worksheet,
             questions: list[WrittenTestQuestionData]) -> None:
-        sheet.write('B1', 'вопрос')
-        sheet.write('B2', 'ответ')
+        wrapped_format = book.add_format(WRAPPED_FORMAT)
+        heading_format = book.add_format(HEADING_FORMAT)
+        centered_heading_format = book.add_format(CENTERED_HEADING_FORMAT)
+        right_bordered_format = book.add_format(RIGHT_BORDERED_FORMAT)
+        column_bg_colored_format = book.add_format(COLUMN_BG_COLORED_FORMAT)
+        row_bg_colored_format = book.add_format(ROW_BG_COLORED_FORMAT)
+
+        sheet.set_row(0, None, row_bg_colored_format)
+        sheet.set_row(1, None, book.add_format({
+            **BOTTOM_BORDERED_FORMAT,
+            **ROW_BG_COLORED_FORMAT,
+            }))
+        sheet.set_column(0, 0, 250, column_bg_colored_format) # Column A
+        sheet.set_column(1, 1, 80, book.add_format({
+            **RIGHT_BORDERED_FORMAT,
+            **COLUMN_BG_COLORED_FORMAT,
+            })) # Column B
+
+        sheet.write('B1', 'Вопрос', heading_format)
+        sheet.write('B2', 'Ответ', heading_format)
 
         col_offset = 1
         mark_cells = []
         for question in questions:
-            sheet.write(0, 1 + col_offset, question.question)
-            sheet.write(1, 1 + col_offset, question.answer)
+            sheet.set_column(1 + col_offset, 1 + col_offset, 350)
+            sheet.write(0, 1 + col_offset, question.question, wrapped_format)
+            sheet.write(1, 1 + col_offset, question.answer, wrapped_format)
 
-            sheet.write(0, 1 + col_offset + 1, 'балл')
+            sheet.set_column(1 + col_offset + 1, 1 + col_offset + 1, 45, right_bordered_format)
+            sheet.write(0, 1 + col_offset + 1, 'Балл', centered_heading_format)
             mark_cell = xls_utility.xl_rowcol_to_cell(1, 1 + col_offset + 1)
             mark_cells.append(mark_cell)
             sheet.write(mark_cell, question.max_mark)
 
             col_offset += 2
 
-        sheet.write(0, 1 + col_offset, 'сумма')
+        sheet.set_column(1 + col_offset, 1 + col_offset, 55)
+        sheet.write(0, 1 + col_offset, 'Сумма', centered_heading_format)
         sheet.write(1, 1 + col_offset, f"=SUM({', '.join(mark_cells)})")
 
     def __write_students(self,
+            book: xls.Workbook,
             sheet: xls_worksheet.Worksheet,
             students: list[WrittenTestStudentData],
             question_count: int) -> dict[str, str]:
@@ -170,8 +185,10 @@ class ExcelService():
 
         Returns: student.id-sum_mark_cell map
         """
-        sheet.write('A3', 'студент')
-        sheet.write('B3', 'группа')
+        wrapped_format = book.add_format(WRAPPED_FORMAT)
+        heading_format = book.add_format(HEADING_FORMAT)
+        sheet.write('A3', 'Студент', heading_format)
+        sheet.write('B3', 'Группа', heading_format)
 
         row_offset = 1
         sum_mark_cell_map = dict()
@@ -184,7 +201,7 @@ class ExcelService():
             for i in range(question_count):
                 answer = student.answers[i].value if i < len(student.answers) else ''
 
-                sheet.write(2 + row_offset, 1 + col_offset, answer)
+                sheet.write(2 + row_offset, 1 + col_offset, answer, wrapped_format)
 
                 mark = student.answers[i].mark if i < len(student.answers) else 0
                 mark_cell = xls_utility.xl_rowcol_to_cell(2 + row_offset, 2 + col_offset)
@@ -200,6 +217,37 @@ class ExcelService():
 
             row_offset += 1
         return sum_mark_cell_map
+
+    def __write_summary(self,
+            book: xls.Workbook,
+            sheet: xls_worksheet.Worksheet,
+            groups: list[WrittenTestGroup],
+            mark_cells: dict[str, dict[str, str]]) -> None:
+        sheet.set_column(0, 0, 80) # Column A
+        sheet.set_column(1, 1, 250) # Column B
+        sheet.set_column(2, 2, 45) # Column C
+
+        heading_format = book.add_format(HEADING_FORMAT)
+        centered_heading_format = book.add_format(CENTERED_HEADING_FORMAT)
+
+        row_offset = 0
+        for group in groups:
+            sheet.write(0 + row_offset, 0, group.group, heading_format)
+            sheet.write(0 + row_offset, 1, 'Студент', heading_format)
+            sheet.write(0 + row_offset, 2, 'Балл', centered_heading_format)
+
+            row_offset += 1
+            student_number = 1
+            for student in group.students:
+                mark_cell = mark_cells[str(student.id)]
+
+                sheet.write(0 + row_offset, 0, student_number)
+                sheet.write(0 + row_offset, 1, student.name)
+                sheet.write_formula(0 + row_offset, 2, f"'{mark_cell['sheet']}'!{mark_cell['mark_cell']}")
+
+                student_number += 1
+                row_offset += 1
+            row_offset += 1
 
 
     # endregion
